ca6408b4dde167b81d36eccc1720748f5db41955
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-01-09 19:35:21 -0700

    Add files via upload

---
+++ Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //Reference player position and camera offset value
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    //Reference player position and text to update
+    public Transform player;
+    public Text scoreText;
+
+    //Update called once per frame
+    void Update()
+    {
+        //Update text with player position whole number
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
